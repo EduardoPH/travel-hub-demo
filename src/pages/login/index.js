250c49ac3ef0a8f09c712146539d92b2df33d987
EduardoPH <eduardo.mendess@icloud.com>
2025-03-18 22:03:13 -0300

    Add login function

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,1 +1,13 @@
-alert('Hello, world!');
+const login = () => {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if (email === 'user@test' && password === '123') {
+    alert('Login success');
+    document.location.href = '/home';
+
+  }
+  else {
+    alert('Login failed');
+  }
+}
